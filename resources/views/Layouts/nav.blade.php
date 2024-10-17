4211bf716730729490cc05a68ce65205784a8e2f
jpgerber1986 <jordangerber@gmail.com>
2024-10-17 13:58:48 -0600

    Update nav - added link. Rearranged

--- resources/views/Layouts/nav.blade.php
+++ resources/views/Layouts/nav.blade.php
@@ -1,7 +1,8 @@
 <nav>
     <div class="container">
         <x-link href='/' icon="fa fa-home">Home</x-link>
-        <x-link href='/connect' icon='fa fa-rocket'>Start</x-link>
+        <x-link href='/connect' icon='fa fa-rocket'>Connect</x-link>
+        <x-link href='/miners' icon='fas fa-user-astronaut'>Top 10</x-link>
         <x-link href='/blocks' icon='fa fa-cubes'>Blocks</x-link>
         <x-link href='/payments' icon='nf nf-md-currency_sign'>Payments</x-link>
 
